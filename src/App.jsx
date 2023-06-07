5049f4adae3df07ce195730793a6c8d2a5298f0c
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-07 12:27:43 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './reset.css'
 import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { Like } from './components/Like/Like';
+import { Notif } from './components/Notif/Notif';
+import { Cart } from './components/Cart/Cart';
+import { Header } from './components/Header/Header';
+import { Account } from './components/Account/Account';
 
 function App() {
-  return (
+
+
+  return (    
     <>  
-      
+     
     </>
   );
 }
 
 export default App;
+
