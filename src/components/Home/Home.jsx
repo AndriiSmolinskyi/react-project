08a5982d446fb9f06431bb257a4e7cf155ebad7e
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-10 21:38:57 +0300

    1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,80 @@
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { SearchBar } from './SearchBar/SearchBar';
+// import './Home.scss';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+// library.add(faMagnifyingGlass);
+
+// export const Home = () => {
+//   const [shoesData, setShoesData] = useState([]);
+//   const [filteredShoes, setFilteredShoes] = useState([]);
+//   const [visibleItems, setVisibleItems] = useState(12);
+//   const [lowestPrice, setLowestPrice] = useState([])
+
+//   const itemsPerLoad = 12;
+
+//   useEffect(() => {
+//     axios.get('./json/api.json').then((res) => {
+//       const { sneakers } = res.data;
+//       setShoesData(sneakers);
+//       setFilteredShoes(sneakers.slice(0, visibleItems));
+//     });
+//   }, [visibleItems]);
+
+//   const handleSearch = (query) => {
+//     const searchQueries = query.toLowerCase().split(' ');
+
+//     const filteredData = shoesData.filter((item) => {
+//       const itemName = item.name.toLowerCase();
+//       const itemBrand = item.brand_name.toLowerCase();
+
+//       return searchQueries.every((searchQuery) =>
+//         itemName.includes(searchQuery) || itemBrand.includes(searchQuery)
+//       );
+//     });
+
+//     setFilteredShoes(filteredData.slice(0, visibleItems));
+//   };
+
+//   const handleLoadMore = () => {
+//     setVisibleItems((prevVisibleItems) => prevVisibleItems + itemsPerLoad);
+//   };
+
+//   return (
+//     <div className="home">
+//       <h1 className='home__title'>Hype Sneakers</h1>
+//       <div className="home__nav">
+//         <div className='input__box'>
+//           <SearchBar onSearch={handleSearch} className="input__search"/>         
+//           <div className='input__icon-block'><FontAwesomeIcon icon={faMagnifyingGlass} /></div>
+//         </div>        
+//       </div>
+      
+//       <div className="home__shoes">
+//         {filteredShoes.map((item) => (
+//           <div key={item.id} className="home__shoes__item">
+//             <div className='home__img__block'>
+//               <img src={item.grid_picture_url} alt="" />
+//             </div>
+//             <div>{item.name}</div>
+//             <div>price: {item.retail_price_cents / 100}$</div>
+//           </div>
+//         ))}
+//       </div>
+//       <div className="home__btn">
+//         {filteredShoes.length < shoesData.length && (
+//           <button onClick={handleLoadMore} className="home__shoes__button">
+//             Load More
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { SearchBar } from './SearchBar/SearchBar';
@@ -5,14 +82,16 @@ import './Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { SortingButtons } from './SortingButtons/SortingButtons';
+
 library.add(faMagnifyingGlass);
 
 export const Home = () => {
   const [shoesData, setShoesData] = useState([]);
   const [filteredShoes, setFilteredShoes] = useState([]);
   const [visibleItems, setVisibleItems] = useState(12);
- 
-  const itemsPerLoad = 20;
+
+  const itemsPerLoad = 12;
 
   useEffect(() => {
     axios.get('./json/api.json').then((res) => {
@@ -29,8 +108,8 @@ export const Home = () => {
       const itemName = item.name.toLowerCase();
       const itemBrand = item.brand_name.toLowerCase();
 
-      return searchQueries.every((searchQuery) =>
-        itemName.includes(searchQuery) || itemBrand.includes(searchQuery)
+      return searchQueries.every(
+        (searchQuery) => itemName.includes(searchQuery) || itemBrand.includes(searchQuery)
       );
     });
 
@@ -41,20 +120,42 @@ export const Home = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + itemsPerLoad);
   };
 
+  const handleFilter = (type) => {
+    let sortedData = [...filteredShoes];
+
+    if (type === 'lowest') {
+      sortedData.sort((a, b) => a.retail_price_cents - b.retail_price_cents);
+    } else if (type === 'highest') {
+      sortedData.sort((a, b) => b.retail_price_cents - a.retail_price_cents);
+    } else if (type === 'newest') {
+      sortedData.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+    } else if (type === 'men') {
+      sortedData = shoesData.filter((item) => item.gender === 'men');
+    } else if (type === 'women') {
+      sortedData = shoesData.filter((item) => item.gender === 'women');
+    }
+
+    setFilteredShoes(sortedData.slice(0, visibleItems));
+  };
+
   return (
     <div className="home">
-      <h1 className='home__title'>Hype Sneakers</h1>
+      <h1 className="home__title">Hype Sneakers</h1>
       <div className="home__nav">
-        <div className='input__box'>
-          <SearchBar onSearch={handleSearch} className="input__search"/>         
-          <div className='input__icon-block'><FontAwesomeIcon icon={faMagnifyingGlass} /></div>
-        </div>        
+        <div className="input__box">
+          <SearchBar onSearch={handleSearch} className="input__search" />
+          <div className="input__icon-block">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+        </div>
       </div>
-      
+
+      <SortingButtons handleFilter={handleFilter} />
+
       <div className="home__shoes">
         {filteredShoes.map((item) => (
           <div key={item.id} className="home__shoes__item">
-            <div className='home__img__block'>
+            <div className="home__img__block">
               <img src={item.grid_picture_url} alt="" />
             </div>
             <div>{item.name}</div>
--- src/components/Home/SortingButtons/SortingButtons.jsx
+++ src/components/Home/SortingButtons/SortingButtons.jsx
@@ -1,66 +1,23 @@
-
 import React from 'react';
-import './SortingButtons.scss'
-export const SortingButtons = ({
-  selectedSize,
-  setSelectedSize,
-  selectedGender,
-  setSelectedGender,
-  selectedColor,
-  setSelectedColor,
-  sortType,
-  setSortType,
-  uniqueSizes,
-  uniqueColors,
-}) => {
-  const handleSizeChange = (event) => {
-    setSelectedSize(event.target.value);
-  };
 
-  const handleGenderChange = (event) => {
-    setSelectedGender(event.target.value);
-  };
-
-  const handleColorChange = (event) => {
-    setSelectedColor(event.target.value);
-  };
-
-  const handleSortChange = (event) => {
-    setSortType(event.target.value);
-  };
-
+export const SortingButtons = ({ handleFilter }) => {
   return (
-    <div className="sorting-buttons">
-      <select value={selectedSize} onChange={handleSizeChange}>
-        <option value="">Select Size</option>
-        {uniqueSizes.map((size) => (
-          <option key={size} value={size}>
-            {size}
-          </option>
-        ))}
-      </select>
-
-      <select value={selectedGender} onChange={handleGenderChange}>
-        <option value="">Select Gender</option>
-        <option value="men">men</option>
-        <option value="women">women</option>
-      </select>
-
-      <select value={selectedColor} onChange={handleColorChange}>
-        <option value="">Select Color</option>
-        {uniqueColors.map((color) => (
-          <option key={color} value={color}>
-            {color}
-          </option>
-        ))}
-      </select>
-
-      <select value={sortType} onChange={handleSortChange}>
-        <option value="">Sort By</option>
-        <option value="newest">Newest Deals</option>
-        <option value="lowestPrice">Lowest Price</option>
-        <option value="highestPrice">Highest Price</option>
-      </select>
+    <div className="filter__box">
+      <button className="filter__button" onClick={() => handleFilter('lowest')}>
+        Lowest Price
+      </button>
+      <button className="filter__button" onClick={() => handleFilter('highest')}>
+        Highest Price
+      </button>
+      <button className="filter__button" onClick={() => handleFilter('newest')}>
+        Newest Deals
+      </button>
+      <button className="filter__button" onClick={() => handleFilter('men')}>
+        Men
+      </button>
+      <button className="filter__button" onClick={() => handleFilter('women')}>
+        Women
+      </button>
     </div>
   );
 };
