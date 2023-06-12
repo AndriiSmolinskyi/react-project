808b492d546cd9aa272d957f1150dae173854fbd
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-12 19:48:16 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './reset.css'
 import './App.scss';
+import { Route, Switch } from 'react-router';
 
 function App() {
   return (
     <>  
-      
+      <Switch>
+        <Route />
+      </Switch>
     </>
   );
 }
