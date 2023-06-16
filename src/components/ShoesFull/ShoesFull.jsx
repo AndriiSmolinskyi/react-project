b96d09a17a6cea4f8095460f10e44452c4caa74a
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-16 14:19:13 +0300

    1

--- src/components/ShoesFull/ShoesFull.jsx
+++ src/components/ShoesFull/ShoesFull.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export const ShoesFull = () => {
   const { id } = useParams();
+  const [shoe, setShoe] = useState(null);
+
+  useEffect(() => {
+    axios.get('/json/api.json').then(res => {
+      const shoesData = res.data.sneakers;
+      const selectedShoe = shoesData.find(item => item.id === parseInt(id));
+      setShoe(selectedShoe);
+    });
+  }, [id]);
 
   const handleAddToCart = () => {
     const userId = localStorage.getItem('userId');
     const cartItem = {
-      id: id,
-      name: 'Sample Item',
-      price: 100,
+      id: shoe.id,
+      name: shoe.name,
+      price: shoe.retail_price_cents,
     };
 
     // Отримати збережену корзину користувача з localStorage або створити нову, якщо корзина порожня
@@ -20,10 +31,17 @@ export const ShoesFull = () => {
     console.log('Item added to cart:', cartItem);
   };
 
+  if (!shoe) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Shoes Full</h1>
       <p>Id: {id}</p>
+      <p>Name: {shoe.name}</p>
+      <p>Price: {shoe.retail_price_cents}</p>
+      <img src={shoe.main_picture_url} alt={shoe.name} />
       <button onClick={handleAddToCart}>Add to Cart</button>
     </div>
   );
