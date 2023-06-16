f69088e08b33fecf48d26501b8fe54a83539b5a5
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-17 01:02:55 +0300

    2-end

--- src/components/Account/Order/Order.jsx
+++ src/components/Account/Order/Order.jsx
@@ -1,78 +1,33 @@
-// import React from 'react';
-
-// export const Order = () => {
-//   const order = localStorage.getItem('order');
-//   const orderItems = (order && JSON.parse(order).items) || [];
-//   const source = (order && JSON.parse(order).source) || 'Warsaw';
-//   const destination = (order && JSON.parse(order).destination) || 'City, Street';
-
-//   const totalPrice = orderItems.reduce(
-//     (total, item) => total + Number(item.price) * item.quantity,
-//     0
-//   );
-
-//   return (
-//     <div>
-//       <h1>Order</h1>
-//       <p>From: {source}</p>
-//       <p>To: {destination}</p>
-//       {orderItems.length > 0 ? (
-//         <div>
-//           <ul>
-//             {orderItems.map(item => (
-//               <li key={`${item.id}_${item.size}`}>
-//                <img src={item.picture} alt="" />
-//                {console.log(item.picture)}
-//                {console.log(item.name)}
-//                {item.name} - Quantity: {item.quantity} - Price: {item.price/100}
-//               </li>
-//             ))}
-//           </ul>
-//           <p>Total Price: {totalPrice / 100}</p>
-//         </div>
-//       ) : (
-//         <p>Your order is empty</p>
-//       )}
-//     </div>
-//   );
-// };
-
-
-
-
-import React from 'react';
 
 export const Order = () => {
-  const order = localStorage.getItem('order');
-  const orderItems = (order && JSON.parse(order).items) || [];
-  const source = (order && JSON.parse(order).source) || 'Warsaw';
-  const destination = (order && JSON.parse(order).destination) || 'City, Street';
+   const userId = localStorage.getItem('userId');
+   const orders = JSON.parse(localStorage.getItem(`orders_${userId}`)) || [];
+ 
+   return (
+     <div>
+       <h1>Order</h1>
+       {orders.length > 0 ? (
+         <div>
+           {orders.map(order => (
+             <div key={order.id}>
+               <p>From: {order.source}</p>
+               <p>To: {order.destination}</p>
+               <ul>
+                 {order.items.map(item => (
+                   <li key={`${item.id}_${item.size}`}>
+                     <img src={item.picture} alt="" />
+                     {item.name} - Quantity: {item.quantity} - Price: {(item.price*item.quantity) / 100}
+                   </li>
+                 ))}
+               </ul>
+               
+             </div>
+           ))}
+         </div>
+       ) : (
+         <p>Your order is empty</p>
+       )}
+     </div>
+   );
+ };
 
-  const totalPrice = orderItems.reduce(
-    (total, item) => total + Number(item.price) * item.quantity,
-    0
-  );
-
-  return (
-    <div>
-      <h1>Order</h1>
-      <p>From: {source}</p>
-      <p>To: {destination}</p>
-      {orderItems.length > 0 ? (
-        <div>
-          <ul>
-            {orderItems.map(item => (
-              <li key={`${item.id}_${item.size}`}>
-               <img src={item.picture} alt="" />
-               {item.name} - Quantity: {item.quantity} - Price: {item.price/100}
-              </li>
-            ))}
-          </ul>
-          <p>Total Price: {totalPrice / 100}</p>
-        </div>
-      ) : (
-        <p>Your order is empty</p>
-      )}
-    </div>
-  );
-};
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,127 +1,4 @@
 
-// import React, { useState, useEffect } from 'react';
-// import { useAuth } from 'hooks/use-auth';
-// import { useNavigate } from 'react-router-dom';
-// import { CartItem } from './CartItem/CartItem';
-// import './Cart.scss';
-// import { Delivery } from './Delivery/Delivery';
-
-// export const Cart = () => {
-//   const [cartItems, setCartItems] = useState([]);
-//   const { isAuth } = useAuth();
-//   const navigate = useNavigate();
-//   const [showDelivery, setShowDelivery] = useState(false);
-
-//   useEffect(() => {
-//     const userId = localStorage.getItem('userId');
-//     const userCart = JSON.parse(localStorage.getItem(`cart_${userId}`)) || [];
-//     setCartItems(userCart);
-//   }, []);
-
-//   const handleRemoveItem = (itemId, size) => {
-//     const updatedCart = cartItems.filter(
-//       item => item.itemId !== itemId || item.size !== size
-//     );
-//     setCartItems(updatedCart);
-//     const userId = localStorage.getItem('userId');
-//     localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedCart));
-//     console.log('Item removed from cart:', itemId);
-//   };
-
-//   const handleIncrement = (itemId, size) => {
-//     const updatedCart = cartItems.map(item => {
-//       if (item.itemId === itemId && item.size === size) {
-//         return { ...item, quantity: item.quantity + 1 };
-//       }
-//       return item;
-//     });
-//     setCartItems(updatedCart);
-//     const userId = localStorage.getItem('userId');
-//     localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedCart));
-//   };
-
-//   const handleDecrement = (itemId, size) => {
-//     const updatedCart = cartItems.map(item => {
-//       if (item.itemId === itemId && item.size === size && item.quantity > 1) {
-//         return { ...item, quantity: item.quantity - 1 };
-//       }
-//       return item;
-//     });
-//     setCartItems(updatedCart);
-//     const userId = localStorage.getItem('userId');
-//     localStorage.setItem(`cart_${userId}`, JSON.stringify(updatedCart));
-//   };
-
-//   const getTotalPrice = () => {
-//     return cartItems.reduce(
-//       (total, item) => total + Number(item.price) * item.quantity,
-//       0
-//     );
-//   };
-
-//   const clickLogin = () => {
-//     navigate('/login');
-//   };
-
-//   const handleBuy = () => {
-//     setShowDelivery(true);
-//   };
-
-//   const handleDeliverySubmit = deliveryData => {
-//     const order = {
-//       items: cartItems,
-//       source: 'Варшава',
-//       destination: `${deliveryData.country}, ${deliveryData.city}, ${deliveryData.street}`,
-//     };
-//     console.log('Order:', order);
-//     // Очистити корзину після оформлення замовлення
-//     setCartItems([]);
-//     const userId = localStorage.getItem('userId');
-//     localStorage.setItem(`cart_${userId}`, JSON.stringify([]));
-//   };
-
-//   if (!isAuth) {
-//     return (
-//       <div>
-//         <h1>Cart</h1>
-//         <p>Please log in to view your cart items.</p>
-//         <button onClick={clickLogin}>Log In</button>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       <h1>Cart</h1>
-//       {cartItems.length > 0 ? (
-//         <div>
-//           <ul>
-//             {cartItems.map(item => (
-//               <CartItem
-//                 key={`${item.id}_${item.size}`}
-//                 item={item}
-//                 handleDecrement={handleDecrement}
-//                 handleIncrement={handleIncrement}
-//                 handleRemoveItem={handleRemoveItem}
-//               />
-//             ))}
-//           </ul>
-//           <div>
-//             <h3>Total Price: {getTotalPrice() / 100}</h3>
-//             <button onClick={handleBuy}>Buy</button>
-//           </div>
-//           {showDelivery && <Delivery handleSubmit={handleDeliverySubmit} />}
-//         </div>
-//       ) : (
-//         <p>Your cart is empty.</p>
-//       )}
-//     </div>
-//   );
-// }
-
-
-
-
 import React, { useState, useEffect } from 'react';
 import { useAuth } from 'hooks/use-auth';
 import { useNavigate } from 'react-router-dom';
@@ -191,16 +68,19 @@ export const Cart = () => {
   };
 
   const handleDeliverySubmit = deliveryData => {
+    const userId = localStorage.getItem('userId');
     const order = {
+      id: Date.now(),
       items: cartItems,
-      source: 'Варшава',
+      source: 'Warsaw',
       destination: `${deliveryData.country}, ${deliveryData.city}, ${deliveryData.street}`,
     };
-    localStorage.setItem('order', JSON.stringify(order)); // Зберігаємо замовлення в localStorage
+    const previousOrders = JSON.parse(localStorage.getItem(`orders_${userId}`)) || [];
+    const updatedOrders = [...previousOrders, order];
+    localStorage.setItem(`orders_${userId}`, JSON.stringify(updatedOrders));
+
     console.log('Order:', order);
-    // Очистити корзину після оформлення замовлення
     setCartItems([]);
-    const userId = localStorage.getItem('userId');
     localStorage.setItem(`cart_${userId}`, JSON.stringify([]));
   };
 
@@ -235,10 +115,15 @@ export const Cart = () => {
             <button onClick={handleBuy}>Buy</button>
           </div>
           {showDelivery && <Delivery handleSubmit={handleDeliverySubmit} />}
+          
         </div>
       ) : (
         <p>Your cart is empty.</p>
       )}
     </div>
   );
-};
+}
+
+
+
+
