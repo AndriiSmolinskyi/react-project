71bbc631b1efe212d763e2e2ebaec04cc9f189e0
AndriiSmolinskyi <awful.braid@gmail.com>
2023-06-17 00:00:08 +0300

    2

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,3 +1,40 @@
+// import React, { useState } from 'react';
+// import { useAuth } from 'hooks/use-auth';
+// import { useDispatch } from 'react-redux';
+// import { removeUser } from 'store/slices/userSlice';
+// import { Order } from './Order/Order';
+// import { Navigate } from 'react-router-dom';
+
+// export const Account = () => {
+//   const { isAuth, email } = useAuth();
+//   const dispatch = useDispatch();
+//   const [showOrder, setShowOrder] = useState(false);
+
+//   const handleLogout = () => {
+//     dispatch(removeUser());
+//   };
+
+//   const handleOrderClick = () => {
+//     setShowOrder(true);
+//   };
+
+//   if (!isAuth) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Welcome</h1>
+//       <button onClick={handleLogout}>Log out from {email}</button>
+//       <button onClick={handleOrderClick}>Your order</button>
+//       <Order />
+//       {/* {showOrder && <Order />} */}
+//     </div>
+//   );
+// };
+
+
+
 import React, { useState } from 'react';
 import { useAuth } from 'hooks/use-auth';
 import { useDispatch } from 'react-redux';
--- src/components/Account/Order/Order.jsx
+++ src/components/Account/Order/Order.jsx
@@ -1,3 +1,45 @@
+// import React from 'react';
+
+// export const Order = () => {
+//   const order = localStorage.getItem('order');
+//   const orderItems = (order && JSON.parse(order).items) || [];
+//   const source = (order && JSON.parse(order).source) || 'Warsaw';
+//   const destination = (order && JSON.parse(order).destination) || 'City, Street';
+
+//   const totalPrice = orderItems.reduce(
+//     (total, item) => total + Number(item.price) * item.quantity,
+//     0
+//   );
+
+//   return (
+//     <div>
+//       <h1>Order</h1>
+//       <p>From: {source}</p>
+//       <p>To: {destination}</p>
+//       {orderItems.length > 0 ? (
+//         <div>
+//           <ul>
+//             {orderItems.map(item => (
+//               <li key={`${item.id}_${item.size}`}>
+//                <img src={item.picture} alt="" />
+//                {console.log(item.picture)}
+//                {console.log(item.name)}
+//                {item.name} - Quantity: {item.quantity} - Price: {item.price/100}
+//               </li>
+//             ))}
+//           </ul>
+//           <p>Total Price: {totalPrice / 100}</p>
+//         </div>
+//       ) : (
+//         <p>Your order is empty</p>
+//       )}
+//     </div>
+//   );
+// };
+
+
+
+
 import React from 'react';
 
 export const Order = () => {
@@ -35,5 +77,3 @@ export const Order = () => {
     </div>
   );
 };
-
-
